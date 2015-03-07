495eedd5b71b1cda3a0a710384b6b255c11f35b4
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2015-03-07 21:21:34 +0100

    Improve positionning

--- board.go
+++ board.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bmatsuo/hexgrid"
 	"github.com/bmatsuo/hexgrid/hexcoords"
 	"github.com/tbruyelle/fsm"
+	"log"
 )
 
 const (
@@ -15,14 +16,15 @@ const (
 
 func NewBoard() {
 	grid := hexgrid.NewGrid(5, 5, hexRadius, nil, nil, nil)
+	xoffset, yoffset := float32(hexWidth*5/2), float32(hexHeight*5/2)
 	for u := grid.ColMin(); u <= grid.ColMax(); u++ {
 		for v := grid.RowMin(); v <= grid.RowMax(); v++ {
 			var (
 				c    = hexcoords.Hex{u, v}
 				tile = grid.GetTile(c)
 				n    = &fsm.Object{
-					X:      hexWidth * float32(u),
-					Y:      hexHeight * float32(v),
+					X:      float32(tile.Pos.X) + xoffset,
+					Y:      float32(tile.Pos.Y) + yoffset,
 					Width:  hexWidth,
 					Height: hexHeight,
 					Sprite: hexSprite,
@@ -30,6 +32,7 @@ func NewBoard() {
 			)
 			n.Register(scene, eng)
 			tile.Value = n
+			log.Printf("tile %d,%d coord %f,%f", u, v, n.X, n.Y)
 		}
 	}
 
